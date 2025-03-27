4f86f8a4fc86d34addde0cf7324fcbbe15df2b44
Vijayrj07 <vimjay2707@gmail.com>
2025-03-27 11:38:08 +0000

    Added new files

---
+++ basics-02/array.js
@@ -0,0 +1,27 @@
+const myArr=[0, 1, 2 ,3 ,4, 5]
+// console.log(myArr[8])  // undefined
+
+let names=["Vijay","Ayush","Rachit"]
+// console.log(typeof names) // object
+
+let names1= new Array(1,2,3,4)
+// console.log(names1[0])
+// console.log(names1[1])
+
+/****Array Methods***** */
+// names1.push(0)  //adds the element to the back of the array
+// names1.pop()
+// names1.unshift(69) // inserts the element to the start of the array
+
+// myArr.shift() //removes the first element from the array.
+
+// console.log(myArr.includes(0));
+// console.log(myArr);
+// console.log(myArr.indexOf(9)); //-1 means element doesn't exists.
+
+let strArr=myArr.join() //adds all the values of the myArr to strArr and changes the type to strings
+// console.log(strArr)  //string value with comma separated.
+// console.log(myArr);
+// console.log(typeof strArr); //string
+
+// slics and splice
--- basics/date_time.js
+++ basics/date_time.js
@@ -1,11 +1,24 @@
 let myDate=new Date();
-console.log(myDate.toString());
-console.log(myDate.toDateString())
-console.log(myDate.toLocaleString());
-console.log(typeof myDate)  // object
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate)  // object
 
 let createdDate=new Date(2023,0,23); //months in js is started from 0 as january.
-console.log(createdDate.toDateString());
+// console.log(createdDate.toDateString());
+// console.log(createdDate.toLocaleDateString());  // similar to LocaleString but removes the time from it.
+// console.log(createdDate.toLocaleString());
 
+let myTimeStamp=Date.now();
+console.log(Math.floor(Date.now()/1000))  //converts the ms to seconds
+console.log(myTimeStamp);
 
-let myTimeStamp=Date.new()
+// console.log(createdDate.getTime());  //converts the date to milliseconds
+let newDate= new Date()
+console.log(newDate.getMonth()) // month value in numbers staring from 0 as jan
+console.log(newDate.getDay());  // provides day count starting with Monday as 1
+
+console.log(newDate.toLocaleString('Daefault', {
+    weekday: "long",
+    
+})) // there are various functions for the localestring to use in the form of objects.
